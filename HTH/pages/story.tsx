ec44e2e207affe354ea1d643432660120a2633a1
Irina <irina.wang@outlook.com>
2022-10-17 23:57:23 -0700

    change bio to story

---
+++ HTH/pages/story.tsx
@@ -0,0 +1,48 @@
+import Container from '../components/container'
+// import MoreStories from '../components/more-stories'
+// import HeroPost from '../components/hero-post'
+import Intro from '../components/main-page/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import ArticleList from '../components/article-list/article-list'
+import YetToCome from '../components/main-page/yet2come'
+
+export default function Index({ allPosts }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro>
+            <span className="font-mono">Story.</span>
+          </Intro>
+          <section className="max-w-4xl mx-auto">
+            <YetToCome/>
+            {/* <ArticleList articles={allPosts}/> */}
+          </section>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts('tech', [
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
