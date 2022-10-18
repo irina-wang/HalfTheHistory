2b3b038e24d163dfe16f55864cde575b9676971c
Irina <irina.wang@outlook.com>
2022-10-17 23:56:59 -0700

    modify events and tools card

--- HTH/components/main-page/events.tsx
+++ HTH/components/main-page/events.tsx
@@ -2,9 +2,28 @@ import styles from '../markdown-styles.module.css';
 import Link from 'next/link';
 
 export default function Events() {
-  return <section className={styles.markdown + " max-w-2xl pb-2 md:pb-0"}>
-{/* Here is the home page content */}
-    
-    <h1>Here's the section for the events</h1>
+  return <section className={styles.markdown + " max-w-4xl pb-6 md:pb-0"}>
+    <h2 className="text-center text-2xl font-extrabold text-gray-900">Events</h2>
+    <div className="bg-white">
+      <div className="pt-6">
+       <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+      <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+        <img src="./../../public/assets/img/poster-ayodele.jpg" alt="" className="h-full w-full object-cover object-center"/>
+      </div>
+      <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+        <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
+          <img src="./../../public/assets/img/poster-ayodele.jpg" alt="" className="h-full w-full object-cover object-center"/>
+        </div>
+        <div className="aspect-w-3 aspect-h-2 overflow-hidden rounded-lg">
+          <img src="./../../public/assets/img/poster-ayodele.jpg" alt="" className="h-full w-full object-cover object-center"/>
+        </div>
+      </div>
+      <div className="aspect-w-4 aspect-h-5 sm:overflow-hidden sm:rounded-lg lg:aspect-w-3 lg:aspect-h-4">
+        <img src="./../../public/assets/img/poster-ayodele.jpg" alt="" className="h-full w-full object-cover object-center"/>
+      </div>
+    </div>
+    </div>
+    </div>
+    <h2></h2>
   </section>
 }
